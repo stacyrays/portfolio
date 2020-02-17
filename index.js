e672710c8ed49125458f427162e72ab1d5508da3
“Stacy <stacyrays@gmail.com>
2020-02-17 14:36:25 -0800

    button added with scroll to functionality

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 $(function() {
   TweenLite.to($("header h1"), 1 * 2, {
     opactiy: 1,
-    y: -200,
+    y: -75,
     ease: Circ.easeOut
+  });
+
+  $("header button").on("click", function() {
+    console.log("you clicked the header button");
+    TweenLite.to($("main"), 1 * 2, {
+      y: -700,
+      ease: Circ.easeOut
+    });
   });
 });
