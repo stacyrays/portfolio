e8bd38c1fb9e7a937f24c6c5623abac41cc792ef
“Stacy <stacyrays@gmail.com>
2020-02-17 14:55:33 -0800

    fixed scroll to

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 $(function() {
   TweenLite.to($("header h1"), 1 * 2, {
-    opactiy: 1,
+    opacity: 1,
     y: -75,
     ease: Circ.easeOut
   });
 
   $("header button").on("click", function() {
-    console.log("you clicked the header button");
-    TweenLite.to($("main"), 1 * 2, {
-      y: -700,
-      ease: Circ.easeOut
-    });
+    gsap.to(window, { duration: 2, scrollTo: ".projects" });
   });
 });
