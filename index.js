6a32a6f7075694457d469bc451f8ca25dd563864
“Stacy <stacyrays@gmail.com>
2020-04-09 16:20:12 -0700

    fixed some styles

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-$(function() {
+$(function () {
   TweenLite.to($("header h1"), 1 * 2, {
     opacity: 1,
     y: -75,
-    ease: Circ.easeOut
+    ease: Circ.easeOut,
   });
 
-  $("header button").on("click", function() {
+  $("header .btnheader").on("click", function () {
     gsap.to(window, { duration: 1.5, scrollTo: ".projects" });
   });
 });
